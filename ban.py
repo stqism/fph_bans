a566ac328ec688c0d0660e2923c577eba2702fe4
Sean <sean@tox.im>
2015-12-23 02:35:47 -0800

    old artifacts

--- ban.py
+++ ban.py
@@ -1,39 +1,37 @@
 import os, urllib2, time, gzip, functools, datetime
-from flask import Flask, Response, after_this_request, request, format_date_time
+from flask import Flask, Response, after_this_request, request
 from wsgiref.handlers import format_date_time
 from email.utils import formatdate
 from cStringIO import StringIO as IO
 wraps = functools.wraps
 app = Flask(__name__)
 
-cache = Cache(app,config={'CACHE_TYPE': 'simple'})
-
 user_cache = [0, 0, '']
 
 def cache(expires=None, round_to_minute=False):
     """
     Add Flask cache response headers based on expires in seconds.
-    
+
     If expires is None, caching will be disabled.
     Otherwise, caching headers are set to expire in now + expires seconds
     If round_to_minute is True, then it will always expire at the start of a minute (seconds = 0)
-    
+
     Example usage:
-    
+
     @app.route('/map')
     @cache(expires=60)
     def index():
       return render_template('index.html')
-    
+
     """
     def cache_decorator(view):
         @wraps(view)
         def cache_func(*args, **kwargs):
             now = datetime.datetime.now()
- 
+
             response = make_response(view(*args, **kwargs))
             response.headers['Last-Modified'] = format_date_time(time.mktime(now.timetuple()))
-            
+
             if expires is None:
                 response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
                 response.headers['Expires'] = '-1'
@@ -45,7 +43,7 @@ def cache(expires=None, round_to_minute=False):
 
                 response.headers['Cache-Control'] = 'public'
                 response.headers['Expires'] = format_date_time(time.mktime(expires_time.timetuple()))
- 
+
             return response
         return cache_func
     return cache_decorator
@@ -129,7 +127,7 @@ xmlns:xlink="http://www.w3.org/1999/xlink" width="390" height="335" style="fill:
 """ % (getusers())
 
     return data
-    
+
 @app.route('/')
 @gzipped
 def main():
