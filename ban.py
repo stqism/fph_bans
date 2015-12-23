f3be3968eeb72c9e6c53e1d206def81cf918fb4b
Sean <sean@tox.im>
2015-12-23 00:59:25 -0800

    Ports/etc

--- ban.py
+++ ban.py
@@ -101,4 +101,4 @@ def dark():
     return resp
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8008)
+    app.run(host='0.0.0.0',port=5000)
---
+++ readme.txt
@@ -0,0 +1,3 @@
+1: heroku login
+2: heroku create
+3: git push heroku master
